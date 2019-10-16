2d293a4b7d1011885481d8c157ebcf25ee53b97a
Caspian Baska <caspianbaska@gmail.com>
2019-10-16 12:16:48 +1100

    fix(shard.yml): add missing dependency habitat

--- spec/helper.cr
+++ spec/helper.cr
@@ -5,6 +5,3 @@ require "json"
 # If you have a testing environment, replace this with a test config file
 require "../src/hound-dog"
 require "../src/hound-dog/*"
-
-# Helper methods for testing controllers (curl, with_server, context)
-require "../lib/action-controller/spec/curl_context"
